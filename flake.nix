6c3afc137a60bead0dda16a7fe995411570dbdfd
xopclabs <b9fyg5ei@duck.com>
2025-08-04 21:38:30 +0300

    Updated zmk-lang-switch

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
         inherit shield;
         src = ./.;
         config = "config";  # Explicitly set config directory
-        zephyrDepsHash = "sha256-VELva0uhCruegWwVReiUjohehVPO04vFMrgQQErTgzI=";
+        zephyrDepsHash = "sha256-mYS+5XlKjCy6KaRi22cwUHLXPzKKOF4emFfXppizrQ0=";
         meta = {
           description = "ZMK firmware for ${name}";
           license = nixpkgs.lib.licenses.mit;
